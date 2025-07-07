eb58a9ea27db40cbc7df7f714871e2ec46bd2c59
unknown <jeanphilippecarraze@gmail.com>
2025-07-07 17:08:43 +0200

    Initialize project for multiservices management

---
+++ patient-service/src/main/java/com/mediscreen/diabete_detection_app/patient_service/DiabeteDetectionAppApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabete_detection_app.patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteDetectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteDetectionAppApplication.class, args);
+	}
+
+}
---
+++ patient-service/src/test/java/com/mediscreen/diabete_detection_app/patient_service/DiabeteDetectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabete_detection_app.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteDetectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
